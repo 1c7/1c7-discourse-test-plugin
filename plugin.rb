85019f42781d7374e22735d4224a7740b7b3d63d
1c7 <guokrfans@gmail.com>
2018-04-29 15:26:13 +0800

    part 5 done

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,16 @@
 # about: 演示 plugin 原理
 # version: 1.2.4
 # authors: 郑诚
+# url: https://github.com/1c7/1c7-discourse-test-plugin
+
+
 enabled_site_setting :awesomeness_enabled
 
+# 告诉 Discourse 需要一个链接
+add_admin_route 'purple_tentacle.title', 'purple-tentacle'
 
+
+Discourse::Application.routes.append do
+  get '/admin/plugins/purple-tentacle' => 'admin/plugins#index', constraints: StaffConstraint.new
+end
 
